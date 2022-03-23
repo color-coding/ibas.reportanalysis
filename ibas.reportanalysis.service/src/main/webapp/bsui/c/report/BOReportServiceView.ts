916f11b7eb1ea2cd6f10a6214013554952d672b1
neil.zhou <neil.zhou@avatech.com.cn>
2022-03-23 09:48:05 +0800

    ReportService页面添加报表结果列表操作

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/BOReportServiceView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/BOReportServiceView.ts
@@ -14,6 +14,7 @@ namespace reportanalysis {
             export class BOReportServiceView extends ReportDialogViewView implements app.IReportViewView {
                 /** 选择报表 */
                 selectReportEvent: Function;
+                private tableBar: sap.m.Toolbar;
                 /** 绘制视图 */
                 draw(): any {
                     let that: this = this;
@@ -77,11 +78,86 @@ namespace reportanalysis {
                                 icon: "sap-icon://reset",
                                 press: function (): void {
                                     that.fireViewEvents(that.resetReportEvent);
+                                    that.tableBar.setVisible(false);
                                 }
                             }),
+                            new sap.m.ToolbarSpacer(""),
+                            this.tableBar = new sap.m.Toolbar("", {
+                                visible: false,
+                                content: [
+                                    new sap.m.ToolbarSeparator(""),
+                                    new sap.m.Label("", {
+                                        width: "auto",
+                                        text: ibas.i18n.prop("reportanalysis_fixed_table"),
+                                    }),
+                                    new sap.m.Input("", {
+                                        width: "3rem",
+                                        type: sap.m.InputType.Number,
+                                        textAlign: sap.ui.core.TextAlign.Right,
+                                    }),
+                                    new sap.m.Input("", {
+                                        width: "3rem",
+                                        type: sap.m.InputType.Number,
+                                        textAlign: sap.ui.core.TextAlign.Right,
+                                    }),
+                                    new sap.m.Input("", {
+                                        width: "3rem",
+                                        type: sap.m.InputType.Number,
+                                        textAlign: sap.ui.core.TextAlign.Right,
+                                    }),
+                                    new sap.m.Button("", {
+                                        text: ibas.i18n.prop("shell_apply"),
+                                        type: sap.m.ButtonType.Success,
+                                        press: function (event: sap.ui.base.Event): void {
+                                            let table: sap.ui.table.Table = (<any>that.viewContainer.getContent()[0]);
+                                            if (table instanceof sap.ui.table.Table) {
+                                                let source: any = event.getSource();
+                                                if (source instanceof sap.m.Button) {
+                                                    let toolBar: any = source.getParent();
+                                                    if (toolBar instanceof sap.m.Toolbar) {
+                                                        let count: number = 0;
+                                                        for (let item of toolBar.getContent()) {
+                                                            if (item instanceof sap.m.Input) {
+                                                                let value: number = ibas.numbers.valueOf(item.getValue());
+                                                                if (count === 0) {
+                                                                    table.setFixedColumnCount(value > 0 && value < table.getColumns().length ? value : 0);
+                                                                    count++;
+                                                                } else if (count === 1) {
+                                                                    table.setFixedRowCount(value > 0 && value < table.getRows().length ? value : 0);
+                                                                    count++;
+                                                                } else if (count === 2) {
+                                                                    table.setFixedBottomRowCount(value > 0 && value < table.getRows().length ? value : 0);
+                                                                    count++;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }),
+                                    new sap.m.ToolbarSeparator(""),
+                                    new sap.m.Button("", {
+                                        text: ibas.i18n.prop("reportanalysis_resize_column"),
+                                        type: sap.m.ButtonType.Accept,
+                                        press: function (event: sap.ui.base.Event): void {
+                                            let table: sap.ui.table.Table = (<any>that.viewContainer.getContent()[0]);
+                                            if (table instanceof sap.ui.table.Table) {
+                                                for (let column: number = table.getColumns().length - 1; column >= 0; column--) {
+                                                    table.autoResizeColumn(column);
+                                                }
+                                            }
+                                        }
+                                    }),
+                                ]
+                            })
                         ]
                     }));
                     super.showReport(report);
+                }
+                showResults(table: ibas.DataTable): void {
+                    super.showResults(table);
+                    this.tableBar.setVisible(true);
                 }
             }
         }
