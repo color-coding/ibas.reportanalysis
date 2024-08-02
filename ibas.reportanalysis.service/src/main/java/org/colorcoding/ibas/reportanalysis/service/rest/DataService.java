color-coding/ibas.receiptpayment

支持请求头token

ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/DataService.java
@@ -1,6 +1,7 @@
 package org.colorcoding.ibas.receiptpayment.service.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.OperationResult;
 import org.colorcoding.ibas.businesspartner.bo.internalreconciliation.InternalReconciliation;
+import org.colorcoding.ibas.receiptpayment.MyConfiguration;
 import org.colorcoding.ibas.receiptpayment.bo.assetrecharge.AssetRecharge;
 import org.colorcoding.ibas.receiptpayment.bo.payment.Payment;
 import org.colorcoding.ibas.receiptpayment.bo.receipt.Receipt;
@@ -33,8 +35,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchPayment")
-	public OperationResult<Payment> fetchPayment(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchPayment(criteria, token);
+	public OperationResult<Payment> fetchPayment(Criteria criteria, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.fetchPayment(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -48,8 +51,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("savePayment")
-	public OperationResult<Payment> savePayment(Payment bo, @QueryParam("token") String token) {
-		return super.savePayment(bo, token);
+	public OperationResult<Payment> savePayment(Payment bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.savePayment(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -64,8 +68,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchReceipt")
-	public OperationResult<Receipt> fetchReceipt(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchReceipt(criteria, token);
+	public OperationResult<Receipt> fetchReceipt(Criteria criteria, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.fetchReceipt(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -79,8 +84,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveReceipt")
-	public OperationResult<Receipt> saveReceipt(Receipt bo, @QueryParam("token") String token) {
-		return super.saveReceipt(bo, token);
+	public OperationResult<Receipt> saveReceipt(Receipt bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.saveReceipt(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -95,8 +101,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchAssetRecharge")
-	public OperationResult<AssetRecharge> fetchAssetRecharge(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchAssetRecharge(criteria, token);
+	public OperationResult<AssetRecharge> fetchAssetRecharge(Criteria criteria,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchAssetRecharge(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -110,8 +117,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveAssetRecharge")
-	public OperationResult<AssetRecharge> saveAssetRecharge(AssetRecharge bo, @QueryParam("token") String token) {
-		return super.saveAssetRecharge(bo, token);
+	public OperationResult<AssetRecharge> saveAssetRecharge(AssetRecharge bo,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveAssetRecharge(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -127,8 +135,8 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchInternalReconciliation")
 	public OperationResult<InternalReconciliation> fetchInternalReconciliation(Criteria criteria,
-			@QueryParam("token") String token) {
-		return super.fetchInternalReconciliation(criteria, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchInternalReconciliation(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -143,8 +151,8 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveInternalReconciliation")
 	public OperationResult<InternalReconciliation> saveInternalReconciliation(InternalReconciliation bo,
-			@QueryParam("token") String token) {
-		return super.saveInternalReconciliation(bo, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveInternalReconciliation(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//

color-coding/ibas.reportanalysis

ibas.reportanalysis.service/src/main/java/org/colorcoding/ibas/reportanalysis/service/rest/DataService.java
@@ -1,6 +1,7 @@
 package org.colorcoding.ibas.reportanalysis.service.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.OperationResult;
 import org.colorcoding.ibas.bobas.data.DataTable;
+import org.colorcoding.ibas.reportanalysis.MyConfiguration;
 import org.colorcoding.ibas.reportanalysis.bo.report.Report;
 import org.colorcoding.ibas.reportanalysis.bo.reportbook.ReportBook;
 import org.colorcoding.ibas.reportanalysis.bo.users.UserReport;
@@ -25,10 +27,8 @@ public class DataService extends BORepositoryReportAnalysis {
 	/**
 	 * 查询-用户报表
 	 * 
-	 * @param user
-	 *            用户
-	 * @param token
-	 *            口令
+	 * @param user  用户
+	 * @param token 口令
 	 * @return 操作结果
 	 */
 	@POST
@@ -36,95 +36,90 @@ public class DataService extends BORepositoryReportAnalysis {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchUserReports")
 	public OperationResult<UserReport> fetchUserReports(@QueryParam("user") String user,
-			@QueryParam("token") String token) {
-		return super.fetchUserReports(user, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchUserReports(user, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
 	 * 运行-用户报表
 	 * 
-	 * @param report
-	 *            用户报表
-	 * @param token
-	 *            口令
+	 * @param report 用户报表
+	 * @param token  口令
 	 * @return 操作结果
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("runUserReport")
-	public OperationResult<DataTable> runUserReport(UserReport report, @QueryParam("token") String token) {
-		return super.runUserReport(report, token);
+	public OperationResult<DataTable> runUserReport(UserReport report,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.runUserReport(report, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
 	/**
 	 * 查询-报表
 	 * 
-	 * @param criteria
-	 *            查询
-	 * @param token
-	 *            口令
+	 * @param criteria 查询
+	 * @param token    口令
 	 * @return 操作结果
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchReport")
-	public OperationResult<Report> fetchReport(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchReport(criteria, token);
+	public OperationResult<Report> fetchReport(Criteria criteria, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.fetchReport(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
 	 * 保存-报表
 	 * 
-	 * @param bo
-	 *            对象实例
-	 * @param token
-	 *            口令
+	 * @param bo    对象实例
+	 * @param token 口令
 	 * @return 操作结果
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveReport")
-	public OperationResult<Report> saveReport(Report bo, @QueryParam("token") String token) {
-		return super.saveReport(bo, token);
+	public OperationResult<Report> saveReport(Report bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.saveReport(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
 	/**
 	 * 查询-报表簿
 	 * 
-	 * @param criteria
-	 *            查询
-	 * @param token
-	 *            口令
+	 * @param criteria 查询
+	 * @param token    口令
 	 * @return 操作结果
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchReportBook")
-	public OperationResult<ReportBook> fetchReportBook(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchReportBook(criteria, token);
+	public OperationResult<ReportBook> fetchReportBook(Criteria criteria,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchReportBook(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
 	 * 保存-报表簿
 	 * 
-	 * @param bo
-	 *            对象实例
-	 * @param token
-	 *            口令
+	 * @param bo    对象实例
+	 * @param token 口令
 	 * @return 操作结果
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveReportBook")
-	public OperationResult<ReportBook> saveReportBook(ReportBook bo, @QueryParam("token") String token) {
-		return super.saveReportBook(bo, token);
+	public OperationResult<ReportBook> saveReportBook(ReportBook bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.saveReportBook(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//